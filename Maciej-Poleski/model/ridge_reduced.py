8f922b07b5b2b872e81bd39c387862dc3ce456f4
Maciej Poleski <d82ks8djf82msd83hf8sc02lqb5gh5@gmail.com>
2015-12-01 21:28:40 +0100

    Results on "normalized" data and work on Decision Trees

--- Maciej-Poleski/model/ridge_reduced.py
+++ Maciej-Poleski/model/ridge_reduced.py
@@ -3,22 +3,21 @@
 import numpy;
 from sklearn import linear_model
 
-data = numpy.loadtxt('input_data_shuffled.csv',delimiter=',')
+data = numpy.loadtxt('input_data_reduced_shuffled.csv',delimiter=',')
 size = len(data)
 testSize = size*80/100
 
-data_X = data[:, [0, 1, 2]]
+data_X = data[:, [0, 1]]
 data_X_train = data_X[:-testSize]
 data_X_test = data_X[-testSize:]
 
-data_Y = data[:,3]
+data_Y = data[:,2]
 data_Y_train = data_Y[:-testSize]
 data_Y_test = data_Y[-testSize:]
 
-regr = linear_model.LinearRegression()
+regr = linear_model.Ridge(alpha=0.2)
 
 regr.fit(data_X_train, data_Y_train)
 
 print('Coefficients: \n', regr.coef_)
-print("Residual sum of squares: %.2f" % numpy.mean((regr.predict(data_X_test) - data_Y_test) ** 2))
 print('Variance score: %.2f' % regr.score(data_X_test, data_Y_test))
