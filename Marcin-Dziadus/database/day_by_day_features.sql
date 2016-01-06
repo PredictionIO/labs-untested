d4d73ab97c6d706da623921f5dcda3ef4a328e59
marcindziadus <dziadus.marcin@gmail.com>
2016-01-06 19:01:01 +0100

    Features that describe the number of ads seen per day created.

---
+++ Marcin-Dziadus/database/day_by_day_features.sql
@@ -0,0 +1,114 @@
+create or replace function init_revenues_table(varchar, int, int) returns void as
+$$
+declare
+  low varchar := concat($2, 'days');
+  up varchar := concat($3, 'days');
+  query varchar := 'create temp table %s as
+                    select conversions.user_id, sum(price*quantity) as %s
+                    from conversions
+                    join users
+                    on conversions.user_id=users.user_id
+                       and conversions.timestamp - users.signup_time >= %s
+                       and conversions.timestamp - users.signup_time < %s
+                    group by conversions.user_id;';
+begin
+   execute format(query, $1, $1, quote_literal(low),quote_literal(up));
+end;
+$$
+language plpgsql;
+
+create or replace function init_views_table(varchar, int, int) returns void as
+$$
+declare
+  low varchar := concat($2, 'days');
+  up varchar := concat($3, 'days');
+  query varchar := 'create temp table %s as
+		    select views.user_id, count(views.user_id) as %s
+		    from views
+                    join users
+                    on views.user_id=users.user_id
+                       and views.timestamp - users.signup_time >= %s
+                       and views.timestamp - users.signup_time < %s
+                    group by views.user_id;';
+begin
+   execute format(query, $1, $1, quote_literal(low),quote_literal(up));
+end;
+$$
+language plpgsql;
+
+create or replace function create_column(varchar) returns varchar as $$
+declare
+  query_create varchar := 'alter table days_features add column %s float;';
+begin
+  return format(query_create, $1, $2);
+end;
+$$
+language plpgsql;
+
+create or replace function update_column(varchar) returns varchar as
+$$
+declare
+  query varchar := 'update %s set %s = %s.%s from %s where %s.user_id = %s.user_id';
+begin
+   return format(query, $1,$2,$2,$2,$2,$2,$2,$1,$2);
+end;
+$$
+language plpgsql;
+
+create or replace function normalize_revenues(varchar) returns void as
+$$
+declare
+  query varchar := 'update days_features set %s = round(coalesce(%s,0)::numeric,3)';
+begin
+   execute format(query, $1, $1);
+end;
+$$
+language plpgsql;
+
+create or replace function normalize_views(varchar) returns void as
+$$
+declare
+  query varchar := 'update days_features set %s = coalesce(%s,0)';
+begin
+   execute format(query, $1, $1);
+end;
+$$
+language plpgsql;
+
+create or replace function create_features(low int, up int) returns void as
+$$
+declare
+  col_name varchar;
+  revenues_name varchar;
+  views_name varchar;
+begin
+  execute 'drop table if exists days_features';
+  execute 'create temp table days_features as select user_id from features';
+  for i in low..up loop
+    col_name := concat('day', i);
+    views_name := concat('views_', col_name);
+    revenues_name := concat('revenues_', col_name);
+    begin
+      execute format('drop table if exists %s', revenues_name);
+      execute format('drop table if exists %s', views_name);
+
+      execute init_revenues_table(col_name, i-1, i);
+      execute init_views_table(col_name, i-1, i);
+
+      execute create_column(views_name);
+      execute create_column(revenues_name);
+
+      execute update_column(views_name);
+      execute update_column(revenues_name);
+
+      execute normalize_views(views_name);
+      execute normalize_revenues(revenues_name);
+    end;
+  end loop;
+end;
+$$
+language plpgsql;
+
+select create_features(1,7);
+\copy (select days_features.*, features.month_revenue from features join days_features on features.user_id=days_features.user_id) to '/tmp/data.csv' with CSV;
+
