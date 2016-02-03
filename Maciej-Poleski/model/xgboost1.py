b960641a4f5a5e2ac0d2b81a21455f909c7354bb
Maciej Poleski <d82ks8djf82msd83hf8sc02lqb5gh5@gmail.com>
2016-02-03 15:14:32 +0100

    Last changes - LOG and score for XGBoost

--- Maciej-Poleski/model/xgboost1.py
+++ Maciej-Poleski/model/xgboost1.py
@@ -6,7 +6,7 @@ import xgboost as xgb
 # size of utmSources = 108
 data = numpy.loadtxt('input_data_reduced4_shuffled_unpacked.csv',delimiter=',')
 size = len(data)
-testSize = size*80/100
+testSize = int(size*80/100)
 
 selection = [i for i in range(123)]
 selection.remove(2)
@@ -21,14 +21,25 @@ data_Y_test = data_Y[-testSize:]
 
 label=[1 if i==2 else 0 for i in range(123)]
 
-dtrain = xgb.DMatrix(data[:-testSize], label=label)
-dtest = xgb.DMatrix(data[-testSize:], label=label)
+dtrain = xgb.DMatrix(data_X_train, label=data_Y_train)
+dtest = xgb.DMatrix(data_X_test, label=data_Y_test)
 
 param = {'objective':'reg:linear' }
+#param = { }
 param['nthread'] = 1
-param['eval_metric'] = 'auc'
+param['eval_metric'] = 'logloss'
+param['lambda']=0.35 # local optimum
 
-evallist = [(dtest,'eval'), (dtrain,'train')]
+evallist = [(dtest,'test'), (dtrain,'train')]
 
 bst = xgb.train(param, dtrain, 10, evallist)
+ypred = bst.predict(dtest)
 
+u=0
+v=0
+mean = numpy.mean(data_Y_test)
+for i in range(testSize):
+    u += (data_Y_test[i]-ypred[i])**2
+    v += (data_Y_test[i]-mean)**2
+
+print('Final score:',1-u/v)
